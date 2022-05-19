8d7773f47cce62db7211a097e8e61dd18f1a2a1a
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-05-19 11:32:50 -0300

    khg

--- TPA13MAI/exercicio4.php
+++ TPA13MAI/exercicio4.php
@@ -1,2 +1,11 @@
 <?php
-$
+
+$num = 5;
+
+if ($num % 2 == 0){
+    echo "Par";
+}else{
+    echo "Ímpar";
+}
+
+?>
--- TPA13MAI/exercicio7.php
+++ TPA13MAI/exercicio7.php
@@ -4,7 +4,7 @@ $salarhr = 55;
 $depend = 30;
 $inss = 0;
 $imprenda = 0;
-
+//Resposta deu R$1940
 $salarbruto = $hr * $salarhr + (50 * $depend);
 
 if ($salarbruto <= 1212){
@@ -13,11 +13,11 @@ if ($salarbruto <= 1212){
     echo $inss = $salarbruto * 0.09 - 18.18;
 } else if ($salarbruto >= 2427.36 && $salarbruto <= 3641.03){
     echo $inss = $salarbruto * 0.12 - 91;
-} else if ($salarbruto >= 3641.04 && $salarbruto <= 7087.22){
+} else ($salarbruto >= 3641.04 && $salarbruto <= 7087.22){
     echo $inss = $salarbruto * 0.14 - 163.82;
 } 
 
-else if ($salarbruto <= 1903.98){
+if ($salarbruto <= 1903.98){
     echo $imprenda = $salarbruto;
 } else if ($salarbruto >= 1903.99 && $salarbruto <= 2826.65){
     echo $imprende = $salarbruto * 0.075 - 142.8;
@@ -25,7 +25,7 @@ else if ($salarbruto <= 1903.98){
     echo $imprenda = $salarbruto * 0.15 - 354.8;
 } else if ($salarbruto >= 3751.06 && $salarbruto <= 4664.68){
     echo $imprenda = $salarbruto * 0.225 - 636.13;
-} else if ($salarbruto >= 4664.68){
+} else ($salarbruto >= 4664.68){
     echo $imprenda = $salarbruto * 0.275 - 869.36;
 }
 
