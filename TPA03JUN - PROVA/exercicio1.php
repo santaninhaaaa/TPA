78f191e3362b7ce75a673d2a6d3dd835fbb41d0f
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-06-03 10:50:24 -0300

    ,jhv

--- TPA03JUN - PROVA/exercicio1.php
+++ TPA03JUN - PROVA/exercicio1.php
@@ -1,13 +1,40 @@
 <?php
-$ = 
-$alitro = 4.9;
-$glitro = 6.7;
-$dlitro = 6.9;
 
+$litros = 15;
+$combus = "A";
+$precos = 0;
+$valor = 0;
 
-if ($alitro <= 50){
-    echo $valorcl - (5/100);
-} 
-else if ($alitros > 50){
-    echo $valorcl - (10/100);
+if ($combus == "A"){
+    $precos = $litros * 2;
+
+if ($litros <= 50){
+     $valor = $precos - 5/100;
 }
+else ($litros > 50){
+     $valor = $precos - 10/100;
+}}
+
+if ($combus == "G"){
+    $precos = $litros * 3.5;
+
+if ($litros <= 30){
+    $valor = $precos - 8/100;
+} 
+else ($litros > 30){
+    $valor = $precos - 9/100;
+}}
+
+if ($combus == "D"){
+    $precos = $litros * 5;
+
+if ($litros <= 100){
+    $valor = $precos - 20/100;
+} 
+else ($litros > 100){
+    $valor = $precos - 22/100;
+}}
+
+echo "O valor a ser pago pelo cliente é R$" . $valor;
+
+?>
--- TPA03JUN - PROVA/exercicio2.php
+++ TPA03JUN - PROVA/exercicio2.php
@@ -1,16 +1,18 @@
 <?php 
-$l1 = 12;
+$l1 = 450;
 $l2 = 15;
 $l3 = 15;
 
-if ( ($l1 + $l2 > $l3) && ($l1 + $l3 > $l2) && ($l2 + $l3 > $l1) ){
-   if ($l1 == $l2 && $l2 == $l3)
+if ($l1 == $l2 && $l2 == $l3){
     echo "Equilátero";
-else if ($l1 == $l2 || $l1 == $l3 || $l2 == $l3)
+}
+else if ($l1 == $l2 || $l1 == $l3 || $l2 == $l3){
     echo "Isósceles";
-else if ($l1 != $l2 && $l1 != $l3 && $l2 != $l3)
+}
+else if ($l1 != $l2 && $l1 != $l3 && $l2 != $l3){
     echo "Escaleno";
-}else
+}
+else if ( ($l1 + $l2 > $l3) && ($l1 + $l3 > $l2) && ($l2 + $l3 > $l1) ){
    echo "Não é um triângulo";
-
+}
  ?>
