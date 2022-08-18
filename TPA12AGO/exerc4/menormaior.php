dc899a39f7a5162153d994755c24e012b5716d35
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-08-18 09:57:22 -0300

    done

---
+++ TPA12AGO/exerc4/menormaior.php
@@ -0,0 +1,16 @@
+<?php
+
+$n1 = $_POST['valor1'];
+$n2 = $_POST['valor2'];
+
+if ($n1 > $n2) {
+    echo "$n1 é maior que $n2";
+}
+else if ($n1 < $n2) {
+    echo "$n1 é menor que $n2";
+}
+else if ($n1 == $n2 ) {
+    echo "$n1 e $n2 são iguais";
+}
+
+?>
