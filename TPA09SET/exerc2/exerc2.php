4439807de982360b3582444d5acb48fa46d9a850
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-09-23 09:38:36 -0300

    feito

--- TPA09SET/exerc2/exerc2.php
+++ TPA09SET/exerc2/exerc2.php
@@ -2,16 +2,28 @@
 
 $n = $_POST['num'];
 
-if ($n % 2 == 0) {
-    echo $n . " é divisível por 2";
+if ($n % 2 == 0 && $n % 5 == 0 && $n % 10 == 0) {
+    echo $n . " é divisível por 2, 5 e 10";
+}
+else if ($n % 2 == 0 && $n % 5 == 0) {
+    echo $n . " é divisível por 2 e por 10";
+}
+else if ($n % 2 == 0 && $n % 10 == 0) {
+    echo $n . " é divisível por 2 e por 10";
+}
+else if ($n % 5 == 0 && $n % 10 == 0) {
+    echo $n . " é divisível por 5 e por 10";
+}
+else if ($n % 2 == 0) {
+    echo $n . " é divisível apenas por 2";
 }
 else if ($n % 5 == 0) {
-    echo $n . " é divisível por 5";
+    echo $n . " é divisível apenas por 5";
 }
 else if ($n % 10 == 0) {
-    echo $n . " é divisível por 10";
+    echo $n . " é apenas divisível por 10";
 }
 else {
-    echo $n. " não é divisível por 2, 5 ou 10";
+    echo $n . " não é divisível por 2, ou 5 ou 10";
 }
 ?>
