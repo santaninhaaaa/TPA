512f945b89043176a759fc119551914eba85d0fe
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-09-23 09:26:13 -0300

    feito

--- TPA09SET/exerc2/exerc2.php
+++ TPA09SET/exerc2/exerc2.php
@@ -2,15 +2,16 @@
 
 $n = $_POST['num'];
 
-switch ($n%2 == 0 && $n%5 == 0 && $n%10 == 0) {
-
-    case 1:
-        echo "$n é divisível por 2, 5 e 10";
-    break;
-
-    default:
-        echo "$n não é divisível por 2, 5 e 10";
-    break;
+if ($n % 2 == 0) {
+    echo $n . "é divisível por 2";
 }
-
+else if ($n % 5 == 0) {
+    echo $n . "é divisível por 5";
+}
+else if ($n % 10 == 0) {
+    echo $n . "é divisível por 10";
+}
+else {
+    echo "Não é divisível";
+}
 ?>
